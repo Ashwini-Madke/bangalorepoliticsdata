22a0763d7b899ba6b6bc428c5291a769c34d4f4c
Ashwini <Ashwini@Acer>
2018-06-08 00:50:21 -0700

    stupid fello

---
+++ Political/src/com/politics/bangalore/Regionalparty.java
@@ -0,0 +1,10 @@
+package com.politics.bangalore;
+
+public class Regionalparty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("collecting data about political parties"); 
+	}
+
+}
