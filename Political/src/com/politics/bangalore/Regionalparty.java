b97d9cd43dffeceef548cc4cb9e25c8dd098f507
kirru12 <39980987+kirru12@users.noreply.github.com>
2018-06-08 13:26:00 +0530

    Update Regionalparty.java

--- Political/src/com/politics/bangalore/Regionalparty.java
+++ Political/src/com/politics/bangalore/Regionalparty.java
@@ -5,6 +5,7 @@ public class Regionalparty {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("collecting data about political parties"); 
+		System.out.println("having fun doing this ");
 	}
 
 }
